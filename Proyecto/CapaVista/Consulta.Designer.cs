c970af7b460bb6b0cd198f28ab1bb0f082b4f88d
FernandoCahuex <fcahuexg@miumg.edu.gt>
2025-08-30 20:08:22 -0600

    Creacion de la capacitacion modelo vista controlador

---
+++ Proyecto/CapaVista/Consulta.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace CapaVista
+{
+    partial class Consulta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DgvConsulta = new System.Windows.Forms.DataGridView();
+            this.BtnConsulta = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvConsulta)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DgvConsulta
+            // 
+            this.DgvConsulta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvConsulta.Location = new System.Drawing.Point(44, 157);
+            this.DgvConsulta.Name = "DgvConsulta";
+            this.DgvConsulta.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.DgvConsulta.RowTemplate.Height = 24;
+            this.DgvConsulta.Size = new System.Drawing.Size(697, 272);
+            this.DgvConsulta.TabIndex = 0;
+            // 
+            // BtnConsulta
+            // 
+            this.BtnConsulta.Location = new System.Drawing.Point(600, 61);
+            this.BtnConsulta.Name = "BtnConsulta";
+            this.BtnConsulta.Size = new System.Drawing.Size(110, 66);
+            this.BtnConsulta.TabIndex = 1;
+            this.BtnConsulta.Text = "Consultar";
+            this.BtnConsulta.UseVisualStyleBackColor = true;
+            this.BtnConsulta.Click += new System.EventHandler(this.BtnConsulta_Click);
+            // 
+            // Consulta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnConsulta);
+            this.Controls.Add(this.DgvConsulta);
+            this.Name = "Consulta";
+            this.Text = "Consulta";
+            ((System.ComponentModel.ISupportInitialize)(this.DgvConsulta)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DgvConsulta;
+        private System.Windows.Forms.Button BtnConsulta;
+    }
+}
